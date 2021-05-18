2a06dea743d601ed3047a1df0538ce7f74544f49
Glitch (hello-express) <none>
2021-05-18 21:24:46 +0000

    ⛽️🎿 Checkpoint ./server.js:20520369/3

--- server.js
+++ server.js
@@ -78,8 +78,8 @@ app.get("/editar", (request, response) => {
   AWS.config.update({region:"sa-east-1"});
   var client = new AWS.DynamoDB.DocumentClient();
   
-  var ator = request.query.atores;
-  var filme = request.query.filmes;
+  var ator = request.query.ator;
+  var filme = request.query.filme;
   var valor = request.query.valor;
   
   var params = {
