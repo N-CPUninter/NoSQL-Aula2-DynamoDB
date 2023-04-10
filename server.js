de669faaedf5f492cfc2eb30a32fc04d190aa848
Glitch (hello-express) <none>
2023-04-10 19:49:23 +0000

    🛬🌨 Checkpoint ./server.js:46194318/6233

--- server.js
+++ server.js
@@ -8,17 +8,19 @@ const app = express();
 
 const AWS = require("aws-sdk");
 
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 // our default array of dreams
 const dreams = [
   "teste de aula",
   "Climb a really tall mountain",
-  "Wash the dishes"
+  "Wash the dishes",
 ];
 
 // make all the files in 'public' available
@@ -36,15 +38,11 @@ app.get("/dreams", (request, response) => {
   response.json(dreams);
 });
 
-
-
-
-
 app.get("/listartabelas", (request, response) => {
-  AWS.config.update({region:"sa-east-1"});
+  AWS.config.update({ region: "sa-east-1" });
   var dynamodb = new AWS.DynamoDB();
   var params = {};
-  dynamodb.listTables(params, function(err,data){
+  dynamodb.listTables(params, function (err, data) {
     if (err) {
       console.log(err);
     } else {
@@ -53,131 +51,86 @@ app.get("/listartabelas", (request, response) => {
   });
 });
 
-
-
-
-
-
-
-
-
 // Inserir
 app.get("/inserir", (request, response) => {
-  AWS.config.update({region:"sa-east-1"});
+  AWS.config.update({ region: "sa-east-1" });
   var client = new AWS.DynamoDB.DocumentClient();
   var params = {
     TableName: "Filmes",
     Item: {
-      Atores:"Antônio Fagundes",
-      NomeDoFilme:"Lagoa Azul",
+      Atores: "Antônio Fagundes",
+      NomeDoFilme: "Lagoa Azul",
       Ano: "2018",
-      Duração:"180",
-      Gênero:"Terror"
-    }
-  }
-        
-  client.put(params, function(err,data){
-    if(err){
+      Duração: "180",
+      Gênero: "Terror",
+    },
+  };
+
+  client.put(params, function (err, data) {
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       response.send(data);
     }
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Excluir
 app.get("/excluir", (request, response) => {
-  AWS.config.update({region:"sa-east-1"});
+  AWS.config.update({ region: "sa-east-1" });
   var client = new AWS.DynamoDB.DocumentClient();
   var ator = request.query.ator;
   var filme = request.query.filme;
-  
-   var params = {
-    TableName: "Filmes",
-    Key: {
-      Atores: ator,
-      NomeDoFilme: filme
-    }
-   }
-   
-   client.delete(params, function(err,data){
-    if(err){
-      console.log(err);
-    }else{
-      response.send(data);
-    }
-  });
-});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Editar
-app.get("/editar", (request, response) => {
-  AWS.config.update({region:"sa-east-1"});
-  var client = new AWS.DynamoDB.DocumentClient();
-  
-  var ator = request.query.ator;
-  var filme = request.query.filme;
-  var valor = request.query.valor;
-  
   var params = {
     TableName: "Filmes",
     Key: {
       Atores: ator,
-      NomeDoFilme: filme
+      NomeDoFilme: filme,
     },
-    UpdateExpression: "set #s = :y",
-    ExpressionAttributeNames:{
-      '#s':"Ano"
-    },
-    ExpresionAttributeValues:{
-      ':y':valor
-    }
-  }
-  
-  client.update(params, function(err,data){
-    if(err){
+  };
+
+  client.delete(params, function (err, data) {
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       response.send(data);
     }
   });
-  
 });
 
+// Editar
+app.get("/editar", (request, response) => {
+  AWS.config.update({ region: "sa-east-1" });
+  var client = new AWS.DynamoDB.DocumentClient();
 
+  var ator = request.query.ator;
+  var filme = request.query.filme;
+  var valor = request.query.valor;
 
+  var params = {
+    TableName: "Filmes",
+    Key: {
+      Atores: ator,
+      NomeDoFilme: filme,
+    },
+    UpdateExpression: "set #s = :y",
+    ExpressionAttributeNames: {
+      "#s": "Ano",
+    },
+    ExpresionAttributeValues: {
+      ":y": valor,
+    },
+  };
 
+  client.update(params, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      response.send(data);
+    }
+  });
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
